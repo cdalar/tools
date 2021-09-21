83aba1fa9a332309581162f4cac25d7e93d5e5f8
Cemal Y. Dalar <cemal@dalar.net>
2021-09-21 09:03:14 +0200

    simple script to add you ip to eks whitelist

---
+++ addmyip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+EKS_CLUSTER_NAME=$(/usr/local/bin/aws eks list-clusters --region=eu-central-1 | jq -r .clusters[0])
+echo $EKS_CLUSTER_NAME
+MYIP=$(curl -s 'https://api.ipify.org?format=json' | jq -r .ip | awk '{print $1 "/32"}')
+echo $MYIP
+IPS=$(aws eks describe-cluster --name $EKS_CLUSTER_NAME | jq --arg MYIP "$MYIP" -r '.cluster.resourcesVpcConfig.publicAccessCidrs + [$MYIP] | join(",")')
+aws eks update-cluster-config --region "eu-central-1" --name $EKS_CLUSTER_NAME --resources-vpc-config endpointPublicAccess=true,endpointPrivateAccess=true,publicAccessCidrs=$IPS
